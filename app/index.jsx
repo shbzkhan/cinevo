b18c4a21173f777f47d239fa2a9d211360f03524
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2024-08-28 23:55:03 +0530

    create dashboard

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,38 @@
 import { Link } from 'expo-router'
-import {Text, View } from 'react-native'
+import {ScrollView, Text, View, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import { images } from '../constants'
 const App = () => {
   return (
-    <SafeAreaView>
-    <View className = "flex justify-center items-center">
-      <Text className = " text-3xl font-pblack ">LayoutRoot</Text>
-      <Link href='/home' >Go to home</Link>
-    </View>
+    <SafeAreaView className = "bg-primary h-full">
+    <ScrollView contentContainerStyle={{height: "100%"}} >
+      <View className="w-full justify-center items-center  px-4">
+              <Image
+            source ={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode='contain'
+        />
+        <Image
+          source={images.cards}
+          className="max-w-[380px] w-full h-[300px]"
+          resizeMode='contain'
+        />
+        <View className="relative mt-5">
+          <Text className="text-3xl text-white font-bold text-center">
+            Discover Endless Possibilities with {' '} 
+            <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+            source={images.path}
+            className="w-[136px] h-[15px]absolute -bottom-2 -right-8"
+            resizeMode='contain'
+            />
+        </View>
+        
+        <Text>Where creativity meet inn</Text>
+      </View>
+    </ScrollView> 
     </SafeAreaView>
   )
 }
